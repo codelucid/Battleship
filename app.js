5cea86fb8522c02a8d69e15890a3759585b27e4d
Coop <61427314+codelucid@users.noreply.github.com>
2020-09-08 17:54:37 -0400

    start on javascript

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const userGrid = document.querySelector('.grid-user')
+    const computerGrid = document.querySelector('.grid-computer')
+    const displayGrid = document.querySelector('.grid-display')
+    const ships = document.querySelectorAll('.ship')
+})
