d234d93b3bcb71616f6504e90e1027a4502a1553
Coop <61427314+codelucid@users.noreply.github.com>
2020-09-09 18:18:22 -0400

    beginning to setup the random ships for computer

--- app.js
+++ app.js
@@ -12,4 +12,73 @@ document.addEventListener('DOMContentLoaded', () => {
     const rotateButton = document.querySelector('#rotate')
     const turnDisplay = document.querySelector('#whose-go')
     const infoDisplay = document.querySelector('#info')
+
+    const userSquares = []
+    const computerSquares = []
+
+    const width = 10
+
+    // Create Board
+    function createBoard(grid, squares) {
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div')
+            square.dataset.id = i
+            grid.appendChild(square)
+            squares.push(square)
+            
+        }
+    }
+    createBoard(userGrid, userSquares)
+    createBoard(computerGrid, computerSquares)
+
+    // Ships
+    const shipArray = [
+        {
+            name: 'destroyer',
+            directions: [
+                [0, 1],
+                [0, width]
+            ]
+        },
+        {
+            name: 'submarine',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2]
+            ]
+        },
+        {
+            name: 'cruiser',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2]
+            ]
+        },
+        {
+            name: 'battleship',
+            directions: [
+                [0, 1, 2, 3],
+                [0, width, width*2, width*3]
+            ]
+        },
+        {
+            name: 'carrier',
+            directions: [
+                [0, 1, 2, 3, 4],
+                [0, width, width*2, width*3, width*4]
+            ]
+        }
+
+    ]
+
+    // Paint the Computer's ships in random locations
+    function generateShips(ship) {
+        let randomDirection = Math.floor(Math.random() * ship.directions.length)
+        let current = ship.directions[randomDirection]
+        if (randomDirection === 0) direction = 1
+        if (randomDirection === 1) direction = 10
+        let randomStart = Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction))
+
+        
+    }
 })
